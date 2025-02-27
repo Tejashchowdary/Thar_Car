73f82f526be43cc9c364abdba20c133772f2ea65
Tejash <godavarthitejash@gmail.com>
2025-02-27 12:54:08 +0530

    commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,26 @@
-import './Navbar.css'
+import { useState } from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className='nav'>
+    <nav className="nav">
       <div className="nav-logo">Thar-olution</div>
-      <ul className="nav-menu">
+      
+      {/* Hamburger Menu Icon */}
+      <div className="nav-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </div>
+
+      <ul className={`nav-menu ${menuOpen ? "open" : ""}`}>
         <li>Home</li>
         <li>Explore</li>
         <li>About</li>
-        <li className='nav-contact'>Contact</li>
+        <li className="nav-contact">Contact</li>
       </ul>
+    </nav>
+  );
+};
 
-        
-    </div>
-  )
-}
-
-export default Navbar
+export default Navbar;
